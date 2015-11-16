af841103d905f85b73e623ea60a0f7d68f653137
Ryan Vogan <voganrc@gmail.com>
2015-11-15 19:01:32 -0500

    starting to get radii

--- hurricane_generator/clean.py
+++ hurricane_generator/clean.py
@@ -1,52 +1,48 @@
 import pandas as pd
 import numpy as np
 
-import time
-
-from geography import get_state
-
 data = pd.read_csv('hurdat2.csv', dtype = str)
+geotags = pd.read_csv('geotags.csv', dtype = str)
 num_rows = data.shape[0]
 
 open('hurdat2_cleaned.csv', 'w').close()
 
 row = 0
+reading = 1
 num_hurricanes = 0
 
 def skip_to_next_hurricane():
 	global row
+	global reading
 	global num_hurricanes
 	while row < num_rows and data.ix[row][0][0:2] != 'AL':
 		row = row + 1
+		reading = reading + 1
 	row = row + 1
 	num_hurricanes = num_hurricanes + 1
-	if row < num_rows:
-		print "\r\nHurricane " + str(num_hurricanes) + "/1793"
 
 skip_to_next_hurricane()
 while row < num_rows:
 	if data.ix[row][0][0:2] == 'AL':
 		row = row + 1
 		num_hurricanes = num_hurricanes + 1
-		if row < num_rows:
-			print "\r\nHurricane " + str(num_hurricanes) + "/1793"
-		pass
-	latitude = data.ix[row][4][1:5]
-	longitude = "-" + data.ix[row][5][2:6]
-	state = get_state(latitude, longitude)
-	if state != None:
-		print state
-	else:
-		print "Ocean"
-	if state == "Mississippi" and data.ix[row][3] == "HU":
+	assert geotags.ix[reading-1][0] == str(row)
+	latitude = geotags.ix[reading-1][1]
+	longitude = geotags.ix[reading-1][2]
+	county = geotags.ix[reading-1][3]
+	state = geotags.ix[reading-1][4]
+	if state == "Mississippi" and data.ix[row][3].strip() == "HU":
+		print data.ix[row][3]
 		fileStream = open('hurdat2_cleaned.csv','a')
-		fileStream.write(latitude + "," + longitude + ",")
-		fileStream.write(get_county(latitude, longitude) + ",")
+		fileStream.write(latitude + ",")
+		fileStream.write(longitude + ",")
+		fileStream.write(county + ",")
 		fileStream.write(data.ix[row][6] + ",")
-		fileStream.write(str(mean(int(data.ix[row][8:12]))) + ",")
-		fileStream.write(str(mean(int(data.ix[row][12:16]))) + ",")
-		fileStream.write(str(mean(int(data.ix[row][16:20]))) + "\n")
+		fileStream.write(str(np.mean(map(float,data.ix[row][8:12]))) + ",")
+		fileStream.write(str(np.mean(map(float,data.ix[row][12:16]))) + ",")
+		fileStream.write(str(np.mean(map(float,data.ix[row][16:20]))) + "\n")
 		fileStream.close()
 		skip_to_next_hurricane()
 	else:
 		row = row + 1
+		reading = reading + 1
